18f212769f0b53750705caae2d953322d3d0c9bf
Wayne <121678379+wenyanglyu@users.noreply.github.com>
2025-05-26 20:59:28 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Receipt Processor Demo</Navbar.Brand>
+          <Navbar.Brand href="#home">Receipt Scanner</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
